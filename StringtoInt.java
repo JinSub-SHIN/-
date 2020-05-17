cfef5cff922ddc7ad6b63b9ce95e649c8979f3eb
JinSub-SHIN <hello2323@naver.com>
2020-05-17 19:21:13 +0900

    Lv1 문자열정수로바꾸기

---
+++ StringtoInt.java
@@ -0,0 +1,17 @@
+package algoTest;
+
+public class StringtoInt {
+
+	/*
+	 *  https://programmers.co.kr/learn/courses/30/lessons/12925
+	 */
+	
+	class Solution {
+	    public int solution(String s) {
+	        
+	        int answer = Integer.parseInt(s);        
+	        return answer;
+	    }
+	}
+	
+}
