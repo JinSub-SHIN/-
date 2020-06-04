33d2da6a82031d489b85c06e5ec4952d8fed8fac
JinSub-SHIN <hello2323@naver.com>
2020-06-04 10:26:28 +0900

    LV2 탑

--- Top.js
+++ Top.js
@@ -1,16 +1,14 @@
 /**
  * 
  */
-function solution(heights){
-	var answer = [];
-	for(var i=heights.length-1 ; i>=0 ; i--){
-		for(var j=i ; j>=0 ; j--){
-			if(heights[i] < heights[j]){
-				answer.unshift(j+1);
-				break;
-			}
-			if(j===0) answer.unshift(0);
-		}
-	}
-	return answer;
+function solution(heights) {
+    return heights.map((v, i) => {
+        while (i) {
+            i--;
+            if (heights[i] > v) {
+                return i + 1;
+            }
+        }
+        return 0;
+    });
 }
