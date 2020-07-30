d4d5d80e6047d43ae83e86f28f51245fe5e85d7c
JinSub <hello2323@naver.com>
2020-07-30 09:49:12 +0900

    가장작은수제거하기

---
+++ SortTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SortTest
+{
+    class SortTest
+    {
+        public class Solution
+        {
+            public int[] solution(int[] arr)
+            {
+                int[] originalArray = new int[arr.Length];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    originalArray[i] = arr[i];
+                }
+                Array.Sort(arr);
+                int find = 0;
+                if (arr.Length == 1)
+                {
+                    return new int[] { -1 };
+                }
+                else
+                {
+                    int[] answer = new int[arr.Length - 1];
+                    int check = arr[0];
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (originalArray[i] == check)
+                        {
+                            find = 1;
+                            continue;
+                        }
+                        if (find == 0)
+                        {
+                            answer[i] = originalArray[i];
+                        }
+                        else
+                        {
+                            answer[i - 1] = originalArray[i];
+                        }
+                    }
+                    return answer;
+                }
+            }
+        }
+    }
+}
